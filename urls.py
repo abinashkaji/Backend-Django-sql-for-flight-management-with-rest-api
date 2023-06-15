e17188794d03c7eff75d3e829b2cde1d1f71a42e
Abinash Basnet <abinash@ioe.edu.np>
2023-06-14 22:19:59 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
 path("",views.index,name="index"),
 path("<int:fid>",views.flight,name="flight"),    
-path("<int:fid>/book",views.book,name="book"),
+path("<int:fid>/book",views.book,name="books"),
 ]
