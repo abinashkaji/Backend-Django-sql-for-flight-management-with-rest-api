6c951fb9232928c899662c5cd35948c93be0b815
Abinash Basnet <abinash@ioe.edu.np>
2023-07-19 23:07:30 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Flight, Airport, Passenger
-
+from .models import Flight, Airport, Passenger
 # Register your models here.
 
 admin.site.register(Flight)
